4cf9666a3f198c53978233559ed3926ee1e03373
Treamz <avtoritet1337@gmail.com>
2019-02-16 12:43:53 +0200

    Nadya Update

---
+++ portfolio/nadya/order.php
@@ -0,0 +1,13 @@
+<?
+if (array_key_exists('messageFF', $_POST)) {
+   $to = 'majstrenko.nadya@gmail.com';
+   $subject = 'Заполнена контактная форма с '.$_SERVER['HTTP_REFERER'];
+   $subject = "=?utf-8?b?". base64_encode($subject) ."?=";
+   $message = "Имя: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nIP: ".$_SERVER['REMOTE_ADDR']."\nСообщение: ".$_POST['messageFF'];
+   $headers = 'Content-type: text/plain; charset="utf-8"';
+   $headers .= "MIME-Version: 1.0\r\n";
+   $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n";
+   mail($to, $subject, $message, $headers);
+   echo $_POST['nameFF'];
+}
+?>
